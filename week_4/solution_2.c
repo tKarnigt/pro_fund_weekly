d22f7b25d323539921f23e36dfa7a849f22e6ab7
Krit Tangpinyoputtikhun <88623313+jino1205101@users.noreply.github.com>
2023-02-26 18:07:24 +0700

    Create solution_2.c

---
+++ week_4/solution_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    for(int i = 1;i <= x;i++)
+    {
+        for(int j = 1;j<=x;j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
